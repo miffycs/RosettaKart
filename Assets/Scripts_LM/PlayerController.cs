1bc978613d5bd2835a489d3df747c32dcf72b785
Leslie Meng <lmeng4@bu.edu>
2019-11-17 05:04:29 -0500

    Basic Blocks added

---
+++ Assets/Scripts_LM/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+	public Text countText;
+	public Text winText;
+
+	private int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        SetCountText ();
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag ("Right Answer"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText ();
+        }
+
+        if (other.gameObject.CompareTag ("Wrong Answer"))
+        {
+            other.gameObject.SetActive (false);
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
+        if (count >= 6)
+        {
+            winText.text = "You've LEARN A NEW LANGUAGE!";
+        }
+    }
+}
